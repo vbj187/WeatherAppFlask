5093c70048ed9c93d8746134084364013f6aec4a
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-07-04 17:42:33 +0530

    ui update

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ app = Flask(
 )
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-
 def get_weather(city):
     url = 'https://api.openweathermap.org/data/2.5/weather?q=' +\
         city+'&units=metric&appid=59e0ab2e8daad5a2ee361b329adf4b3f'
@@ -22,10 +17,24 @@ def get_weather(city):
     return {"description": weather_description, "temp": temp}
 
 
-@app.route("/<location>")
-def weather(location):
-    weather_details = get_weather(location)
-    return weather_details
+@app.route("/")
+def weather():
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59e0ab2e8daad5a2ee361b329adf4b3f'
+    city = 'Tiruchirappalli'
+
+    r = requests.get(url.format(city)).json()
+    # print(r)
+
+    weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    print(weather)
+
+    return render_template("weather.html", weather=weather)
 
 
 app.run(port=5000)
