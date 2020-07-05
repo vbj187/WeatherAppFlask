84fdc1f9686149648e6c755f21e82f95c5b2cc1e
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-07-05 11:21:38 +0530

    POST method fixed---conditional rendering needed to be fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(
@@ -8,33 +8,22 @@ app = Flask(
 )
 
 
-def get_weather(city):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=' +\
-        city+'&units=metric&appid=59e0ab2e8daad5a2ee361b329adf4b3f'
-    json_response = requests.get(url).json()
-    weather_description = json_response["weather"][0]["description"]
-    temp = json_response["main"]["temp"]
-    return {"description": weather_description, "temp": temp}
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def weather():
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59e0ab2e8daad5a2ee361b329adf4b3f'
-    city = 'Tiruchirappalli'
-
-    r = requests.get(url.format(city)).json()
-    # print(r)
-
-    weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
-
-    print(weather)
-
-    return render_template("weather.html", weather=weather)
+    if request.method == 'POST':
+        city = request.form['city']
+        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59e0ab2e8daad5a2ee361b329adf4b3f'
+        r = requests.get(url.format(city)).json()
+        weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        print(weather)
+        return render_template('weather.html', weather=weather)
+    else:
+        return render_template('weather.html')
 
 
 app.run(port=5000)
