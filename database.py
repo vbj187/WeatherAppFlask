9b38dbc56c72cb495de281baad329a4dae8191d5
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-07-06 14:11:39 +0530

    table created

--- database.py
+++ database.py
@@ -12,5 +12,22 @@ def create_connection(db_file):
         return None
 
 
+def run_query(sql_query, args=[]):
+    conn = create_connection(db_name)
+    cur = conn.cursor()
+    cur.execute(sql_query, args)
+
+
+def create_table():
+    sql_query = """create table if not exists cities(
+        id INTEGER PRIMARY KEY,
+        url text,
+        website_text text
+    );
+    """
+    run_query(sql_query)
+
+
 if __name__ == '__main__':
     create_connection(db_name)
+    create_table()
